56c50d76f914091c5615829314c123405f1d94c8
Hugo SENETAIRE <hugo.senetaire@louisvuitton.com>
2020-06-23 11:31:03 +0200

    Fix bugs

--- models/networks.py
+++ models/networks.py
@@ -150,6 +150,8 @@ def define_G(input_nc, output_nc, ngf, netG, norm='batch', use_dropout=False, in
         net = ResnetGeneratorBilinear(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, n_blocks=9, dim_year=dim_year)
     elif netG == 'resnet_6blocks':
         net = ResnetGeneratorBilinear(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, n_blocks=6, dim_year=dim_year)
+    elif netG == 'resnet_3blocks':
+        net = ResnetGeneratorBilinear(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, n_blocks=3, dim_year=dim_year)
     # elif netG == 'unet_128':
     #     net = UnetGenerator(input_nc, output_nc, 7, ngf, norm_layer=norm_layer, use_dropout=use_dropout)
     # elif netG == 'unet_256':
