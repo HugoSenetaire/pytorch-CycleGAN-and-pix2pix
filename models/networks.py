cfec15414f2b020cc99e174c1713d6f03211b5f0
Hugo SENETAIRE <hugo.senetaire@louisvuitton.com>
2020-06-23 11:08:33 +0200

    Fix bugs

--- models/networks.py
+++ models/networks.py
@@ -116,7 +116,7 @@ def init_net(net, init_type='normal', init_gain=0.02, gpu_ids=[]):
     return net
 
 
-def define_G(input_nc, output_nc, ngf, netG, norm='batch', use_dropout=False, init_type='normal', init_gain=0.02, gpu_ids=[], dim_year):
+def define_G(input_nc, output_nc, ngf, netG, norm='batch', use_dropout=False, init_type='normal', init_gain=0.02, gpu_ids=[], dim_year=9):
     """Create a generator
 
     Parameters:
