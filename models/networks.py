d49d3983fee60b617c5db24c5c8f68c6921b2f80
Hugo SENETAIRE <hugo.senetaire@louisvuitton.com>
2020-06-24 10:22:36 +0200

    Fix generator

--- models/networks.py
+++ models/networks.py
@@ -349,7 +349,7 @@ class ResnetGeneratorBilinear(nn.Module):
         self.first_layer = nn.Sequential(*first_layer)
                 
         model_down = []
-        n_downsampling = 3
+        n_downsampling = 2
         for i in range(n_downsampling):  # add downsampling layers
             mult = 2 ** i
             model_down += [nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=5, stride=4, padding=2, bias=use_bias),
